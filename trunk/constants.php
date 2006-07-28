d23a3db1a59ccfb3fa4f0fb086bd074c585a8596
dabbaking <dabbaking@5002577d-9819-0410-bc1e-f7a6be81754e>
2006-07-28 18:39:41 +0000

    Added Base Files

---
+++ trunk/constants.php
@@ -0,0 +1,19 @@
+<?
+/**
+ * Constants.php
+ *
+ */
+ 
+/**
+ * Database Constants
+ */
+define("DB_SERVER", "localhost");
+define("DB_USER", "username");
+define("DB_PASS", "pass");
+define("DB_NAME", "dv");
+
+/**
+ * Database Table Constants
+ */
+define("TBL_FOO", "foo");
+?>
